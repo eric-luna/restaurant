f36f3a79d677b5a7ea91a79bf85f315ded98976a
Eric <ericluna1992@gmail.com>
2017-02-20 21:35:01 -0600

    added menu buttons

--- js/app.js
+++ js/app.js
@@ -7,3 +7,46 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var foodButton = document.getElementById('food');
+var drinksButton = document.getElementById('drinks');
+var dessertsButton = document.getElementById('desserts');
+var foodMenu = document.getElementById('food-menu');
+var drinksMenu = document.getElementById('drink-menu');
+var dessertsMenu = document.getElementById('dessert-menu');
+
+foodButton.addEventListener('click', function(){
+	foodButton.classList.add("selected");
+	drinksButton.classList.remove("selected");
+	dessertsButton.classList.remove("selected");
+	foodMenu.classList.add("show");
+	foodMenu.classList.remove("hide");
+	drinksMenu.classList.remove("show");
+	drinksMenu.classList.add("hide");
+	dessertsMenu.classList.remove("show");
+	dessertsMenu.classList.add("hide");
+})
+
+drinksButton.addEventListener('click', function(){
+	drinksButton.classList.add("selected");
+	foodButton.classList.remove("selected");
+	dessertsButton.classList.remove("selected");
+	drinksMenu.classList.add("show", "selected");
+	drinksMenu.classList.remove("hide");
+	foodMenu.classList.remove("show");
+	foodMenu.classList.add("hide");
+	dessertsMenu.classList.remove("show");
+	dessertsMenu.classList.add("hide");
+})
+
+dessertsButton.addEventListener('click', function(){
+	dessertsButton.classList.add("selected");
+	foodButton.classList.remove("selected");
+	drinksButton.classList.remove("selected");
+	dessertsMenu.classList.add("show","selected");
+	dessertsMenu.classList.remove("hide");
+	foodMenu.classList.remove("show", "selected");
+	foodMenu.classList.add("hide");
+	drinksMenu.classList.remove("show", "selected");
+	drinksMenu.classList.add("hide");
+})
